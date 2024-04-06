e037c0c34cd2bd0b1f06b7dd517bdac8c7fc6112
Matteo Ferigo <matteoferigo@gmail.com>
2024-04-06 12:03:29 +0200

    Hook per uso mappe ArcGis

---
+++ src/hooks/map/index.ts
@@ -0,0 +1,43 @@
+import Map from "ol/Map";
+import type MapBrowserEvent from "ol/MapBrowserEvent";
+import View from "ol/View";
+import type { Coordinate } from "ol/coordinate";
+import { ListenerFunction } from "ol/events";
+import type BaseLayer from "ol/layer/Base";
+import { useEffect, useRef } from "react";
+
+type MapHookOptions = {
+  center: Coordinate;
+  zoom: number;
+  layers: BaseLayer[];
+  onClick?(event: MapBrowserEvent<any>): void;
+};
+
+export const useMap = ({ center, zoom, layers, onClick }: MapHookOptions) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Instanzio la mappa
+    const map = new Map({
+      target: ref.current!,
+      view: new View({ center, zoom }),
+      layers,
+    });
+
+    // TEMP: use ArcGis
+    // const basemapId = "arcgis/outdoor";
+    // apply(map, `api/arcgis/styles/${basemapId}`);
+
+    // Aggiungo gli event listener
+    if (onClick) map.addEventListener("click", onClick as ListenerFunction);
+
+    // Rimuovo i listener
+    return () => {
+      map.setTarget(undefined);
+      if (onClick)
+        map.removeEventListener("click", onClick as ListenerFunction);
+    };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return ref;
+};
