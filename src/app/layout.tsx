47971fb2015aabc1d5d788c306becfe17418b7c3
Matteo Ferigo <matteoferigo@gmail.com>
2024-05-20 15:51:49 +0200

    Sopprimo warning extra attributi dal server di Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
 }: Readonly<React.PropsWithChildren>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
